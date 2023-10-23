b8e062e537152d3caeeb0e1283d6498caca520b9
Zhenyu <zyy1120126@gmail.com>
2023-10-24 00:21:59 +0800

    ✨ feat: Add state in application

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,40 @@
-use actix_web::{web, App, HttpResponse, HttpServer};
+// 引入 Actix Web
+use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
+use std::sync::Mutex;
 
-// this function could be located in a different module
-fn scoped_config(cfg: &mut web::ServiceConfig) {
-    cfg.service(
-        web::resource("/test")
-            .route(web::get().to(|| async { HttpResponse::Ok().body("test") }))
-            .route(web::head().to(HttpResponse::MethodNotAllowed)),
-    );
+// This struct represents state
+struct AppState {
+    app_name: String,
 }
 
-// this function could be located in a different module
-fn config(cfg: &mut web::ServiceConfig) {
-    cfg.service(
-        web::resource("/app")
-            .route(web::get().to(|| async { HttpResponse::Ok().body("app") }))
-            .route(web::head().to(HttpResponse::MethodNotAllowed)),
-    );
+struct AppStateWithCounter {
+    counter: Mutex<i32>, // <- Mutex is necessary to mutate safely across threads
+}
+
+#[get("/")]
+async fn index_1(data: web::Data<AppState>) -> String {
+    let app_name = &data.app_name; // <- get app_name
+    format!("Hello {app_name}!") // <- response with app_name
+}
+
+async fn index_2(data: web::Data<AppStateWithCounter>) -> String {
+    let mut counter = data.counter.lock().unwrap(); // <- get counter's MutexGuard
+    *counter += 1; // <- access counter inside MutexGuard
+
+    format!("Request number: {counter}") // <- response with count
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
-        App::new()
-            .configure(config)
-            .service(web::scope("/api").configure(scoped_config))
-            .route(
-                "/",
-                web::get().to(|| async { HttpResponse::Ok().body("/") }),
-            )
+        // App::new().service(
+        //     web::scope("/app")
+        //         .route("/index.html", web::get().to(index)),
+        // )
+        App::new().app_data(web::Data::new(AppState {
+                app_name: String::from("Actix Web"),
+            }))
+            .service(index_1)
     })
     .bind(("127.0.0.1", 8080))?
     .run()
